4bf503b75a600d7c795fd705231a6d550d1b47e5
Tommy Yu <tommy.yu@auckland.ac.nz>
2024-04-25 02:37:30 +1200

    Include legacy docgen, if available.
    - Also changed the argparser to produce the subparsers based on the
      function signature.

--- cellml/pmr2/cli.py
+++ cellml/pmr2/cli.py
@@ -1,6 +1,15 @@
 from argparse import ArgumentParser
+from inspect import getargspec
 from os.path import join
+
 from cellml.api.pmr2.utility import CellMLAPIUtility
+
+try:
+    from pmr2.processor.legacy.transforms import tmpdoc2html
+except ImportError:
+    tmpdoc2html = None
+
+# codegen
 
 codegen_fileext = {
     'Python': 'py',
@@ -9,7 +18,6 @@ codegen_fileext = {
     'F77': 'f77',
     'MATLAB': 'm',
 }
-
 
 def codegen(input_path, output_dir):
     cu = CellMLAPIUtility()
@@ -20,24 +28,45 @@ def codegen(input_path, output_dir):
             fd.write(v)
 
 
-# generate all commands
-commands = {
-    cmd.__name__: cmd for cmd in [
-        codegen,
-    ]
-}
+# legacy tmpdoc
+
+def tmpdoc(input_path, output_dir):
+    with open(input_path) as inc:
+        output = tmpdoc2html(inc)
+        output_file = join(output_dir, 'index.html')
+        with open(output_file, 'w') as fd:
+            fd.write(output.getvalue())
+
+
+# set up commands
+
+cmd_fns = [
+    (codegen, 'CellML codegen')
+]
+
+if tmpdoc2html:
+    cmd_fns.append((tmpdoc, 'CellML legacy tmpdoc'))
+
+def generate_cmd_parser(cmd_fns):
+    commands = {}
+    argparser = ArgumentParser()
+    ap_grp = argparser.add_subparsers(dest='cmd', metavar='<command>')
+
+    for cmd_fn, help_text in cmd_fns:
+        commands[cmd_fn.__name__] = cmd_fn
+        sub_ap = ap_grp.add_parser(cmd_fn.__name__, help=help_text)
+        for arg in getargspec(cmd_fn).args:
+            sub_ap.add_argument(
+                '--' + arg.replace('_', '-'),
+                action='store',
+                required=True,
+            )
+
+    return commands, argparser
 
 
 def main():
-    import sys
-    import json
-    argparser = ArgumentParser()
-    subparser = argparser.add_subparsers(dest='cmd', metavar='<command>')
-
-    codegen_ap = subparser.add_parser('codegen', help='CellML Codegen')
-    codegen_ap.add_argument('--input-path', action='store')
-    codegen_ap.add_argument('--output-dir', action='store')
-
+    commands, argparser = generate_cmd_parser(cmd_fns)
     kwargs = vars(argparser.parse_args())
     cmd_key = kwargs.pop('cmd')
     commands[cmd_key](**kwargs)
